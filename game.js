11ef323c29402d32df650cf4d756323341116559
Konrad Scherb <Konrad.Scher@gmx.de>
2021-09-28 13:35:55 +0200

    Add comparison logic game.js

--- game.js
+++ game.js
@@ -1,1 +1,33 @@
-console.log('This is a test case');
+function computerPlay() {
+    // function computerPlay randomly returns the values
+    // 'Rock', 'Paper' or 'Scissors', i.e. randomly select
+    // a string value from a list of string values
+    const options = ['rock', 'paper', 'scissors'];
+    const random = Math.floor(Math.random() * options.length);
+    const computerOption = options[random];
+
+    return computerOption
+}
+
+
+function playRound(playerOption, computerOption) {
+    /// Comparison logic
+    if (playerOption === computerOption) {
+        alert('Tie!')
+    } else if (playerOption === 'rock' && computerOption === 'paper') {
+        alert('You Loose! Paper beats Rock!');
+    } else if (playerOption === 'paper' && computerOption === 'rock') {
+        alert('You Win! Paper beats Rock!');
+    } else if (playerOption === 'paper' && computerOption === 'scissors') {
+        alert('You Loose! Scissors beats Paper!');
+    } else if (playerOption === 'scissors' && computerOption === 'paper') {
+        alert('You Win! Scissors beats Paper!');
+    }
+}
+
+const playerOption = prompt('Please type in: "Paper", "Rock", or "Scissors"').toLowerCase();
+const computerOption = computerPlay();
+console.log(computerOption);
+
+
+console.log(playRound(playerOption, computerOption));
